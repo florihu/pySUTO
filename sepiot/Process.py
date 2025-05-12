f41ac632dc5d8b492ba476b20b5941ac0ce00e16
Florian Huber <flhuber@wu.ac.at>
2025-05-12 09:40:17 +0200

    soulier data clean

--- sepiot/Process.py
+++
@@ -1,7 +0,0 @@
-class Process:
-    def __init__(self, process_name: str, process_id: int):
-        self.process_name = process_name
-        self.process_id = process_id
-
-    def __repr__(self):
-        return f"PROCESS(process_name={self.process_name}, process_id={self.process_id})"
